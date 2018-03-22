03840b7a69b4dff733ca82c76d773cc94086e0ea
nieroda <eathotlead@gmail.com>
2018-03-22 15:17:18 -0700

    myGroups and Welcome

--- app/src/main/java/com/example/btcpro/dolphons/myGroups.java
+++ app/src/main/java/com/example/btcpro/dolphons/myGroups.java
@@ -5,25 +5,44 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class myGroups extends AppCompatActivity {
+
+    ListView myGroupsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_groups);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        myGroupsListView = findViewById(R.id.myGroupsListView);
+
+        final ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Data");
+        arrayList.add("Data1");
+        arrayList.add("Data2");
+        arrayList.add("Data3");
+        arrayList.add("Data4");
+        arrayList.add("Data5");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        myGroupsListView.setAdapter(arrayAdapter);
+        myGroupsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("Tapped: ", arrayList.get(i));
             }
         });
+
     }
 
 }
