0e2a0c46467fc0a61c2aa9161567d590536cc9e7
Serg <serg@Sergs-MacBook-Air.local>
2018-03-22 20:08:01 -0700

    sergio added a Login

--- app/src/main/java/com/example/btcpro/dolphons/Login.java
+++ app/src/main/java/com/example/btcpro/dolphons/Login.java
@@ -1,7 +1,10 @@
 package com.example.btcpro.dolphons;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,4 +13,19 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-}
+
+
+    public void showGreetings(View view) {
+        String button_text;
+        button_text = ((Button) view).getText().toString();
+
+        if (button_text.equals("Sign In")) {
+            Intent intent = new Intent(this, Login_SignIn.class);
+            startActivity(intent);
+        }
+        else if (button_text.equals("Sign Up")) {
+            Intent intent = new Intent(this, Login_SignUp.class);
+            startActivity(intent);
+        }
+    }
+};
---
+++ app/src/main/java/com/example/btcpro/dolphons/Login_SignIn.java
@@ -0,0 +1,20 @@
+package com.example.btcpro.dolphons;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by serg on 3/22/18.
+ */
+
+public class Login_SignIn extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_signin);
+    }
+
+}
+
+
+
