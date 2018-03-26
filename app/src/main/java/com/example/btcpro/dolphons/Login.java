ba690b7f851b0e7e7ea75a7c972529af1125a2c3
nieroda <eathotlead@gmail.com>
2018-03-26 13:45:57 -0700

    used flex, rework part of login

--- app/src/main/java/com/example/btcpro/dolphons/Login.java
+++ app/src/main/java/com/example/btcpro/dolphons/Login.java
@@ -6,19 +6,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.*;
+
+
+
 public class Login extends AppCompatActivity {
+
+    String email = "test@gmail.com";
+    String password = "abadpassword";
+    String name = "Nate Kamm";
+    //private AuthUI mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Firebase.setAndroidContext(this);
     }
 
 
     public void showGreetings(View view) {
-        String button_text;
-        button_text = ((Button) view).getText().toString();
+        //String button_text;
+        //button_text = ((Button) view).getText().toString();
 
+
+        /*
         if (button_text.equals("Sign In")) {
             Intent intent = new Intent(this, Login_SignIn.class);
             startActivity(intent);
@@ -27,5 +41,6 @@ public class Login extends AppCompatActivity {
             Intent intent = new Intent(this, Login_SignUp.class);
             startActivity(intent);
         }
+        */
     }
 };
