92fd82e1530e4a0da9a2e36699d2ad8a985c0542
nieroda <eathotlead@gmail.com>
2018-03-26 19:32:00 -0700

    added-ish login firebase

--- app/src/main/java/com/example/btcpro/dolphons/Login.java
+++ app/src/main/java/com/example/btcpro/dolphons/Login.java
@@ -1,13 +1,23 @@
 package com.example.btcpro.dolphons;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 //import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.*;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 
@@ -16,6 +26,15 @@ public class Login extends AppCompatActivity {
     String email = "test@gmail.com";
     String password = "abadpassword";
     String name = "Nate Kamm";
+    Boolean signIn = true;
+
+    private FirebaseAuth mAuth;
+
+
+    TextView nameView;
+    TextView emailView;
+    TextView passwordView;
+    Button loginSignupButton;
     //private AuthUI mAuth;
 
 
@@ -23,24 +42,116 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //Firebase.setAndroidContext(this);
+
+        nameView = findViewById(R.id.nameView);
+        emailView = findViewById(R.id.emailView);
+        passwordView = findViewById(R.id.passwordView);
+        loginSignupButton = findViewById(R.id.loginSignupButton);
+
     }
 
 
-    public void showGreetings(View view) {
-        //String button_text;
-        //button_text = ((Button) view).getText().toString();
+    /* one onclick function , manage with ID's  TO DO*/
+    public void loginSignup(View v) {
 
-
-        /*
-        if (button_text.equals("Sign In")) {
-            Intent intent = new Intent(this, Login_SignIn.class);
-            startActivity(intent);
-        }
-        else if (button_text.equals("Sign Up")) {
-            Intent intent = new Intent(this, Login_SignUp.class);
-            startActivity(intent);
-        }
-        */
     }
+
+    public void signUp(View v) {
+        if (signIn) {
+            nameView.setVisibility(View.VISIBLE);
+            loginSignupButton.setText("Sign Up");
+            signIn = !signIn;
+        }
+    }
+
+    public void login(View v) {
+        createAccount(name, password);
+        if (!signIn) {
+            nameView.setVisibility(View.GONE);
+            loginSignupButton.setText("Login");
+            System.out.println("Click");
+            signIn = !signIn;
+        }
+    }
+
+    /* Thanks Google */
+
+    private void createAccount(String email, String password) {
+       // Log.d(TAG, "createAccount:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                           //         Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        //hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END create_user_with_email]
+    }
+
+
+    private void signIn(String email, String password) {
+        //Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return;
+        }
+
+        //showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            //Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                          //  updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                            //        Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                       // if (!task.isSuccessful()) {
+                        //    mStatusTextView.setText(R.string.auth_failed);
+                       // }
+                       // hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    /* Need to add this */
+
+    private boolean validateForm() {
+        return true;
+    }
+
 };
