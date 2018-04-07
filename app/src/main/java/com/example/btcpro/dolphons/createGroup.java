4230f237e446f4ae83e4d8693b56444a3d9c5b3b
Nathan Kamm <eathotlead@gmail.com>
2018-04-06 17:39:58 -0700

    Update createGroup.java

--- app/src/main/java/com/example/btcpro/dolphons/createGroup.java
+++ app/src/main/java/com/example/btcpro/dolphons/createGroup.java
@@ -195,115 +195,39 @@ public class createGroup extends AppCompatActivity
 
 
 /*
-  const { currentUser } = firebase.auth();
-  const { uid } = currentUser;
-  const lower_name = name.toLowerCase();
-  ///users/${currentUser.uid}/groups
-  return async dispatch => {
-    try {
-      //how data is structured
-      dispatch({ type: GROUP_CREATE_PRESS })
-      //const response = await firebase.database().ref(`/groups/${name}${uid}`)
-      const response = await firebase.database().ref(`/groups/`)
-        .push({
-           name,
-          organizer,
-          summary,
-          isPublic,
-          lower_name,
-          owner: uid,
-        })
-      const { key } = response
-      //console.log(key);
-      await firebase.database().ref('/users/${uid}').push({ key })
-      await firebase.database().ref('/events/${key}').push({
-        location: 'Group Created',
-        summary: 'Group Created',
-        chosenDate: new Date().toString()
-      })
-      dispatch({ type: GROUP_CREATE_SUCCESS })
-      return;
-    } catch (e) { console.log(e) }
-  }
 
-  //sign in
-    const user = await firebase.auth().signInWithEmailAndPassword(email, password);
+          const resp = await firestore.collection('groups')
+            .add({
+              name,
+              organizer,
+              summary,
+              isPublic,
+              lower_name,
+              owner: uid,
+              displayName
+            })
+      let { id } = resp;
 
+      const add = await firestore.collection('users')
+          .doc(uid)
+          .collection('groupsApartOf')
+          .add({ groupID: id, name, organizer });
 
-//create user
-const user = await firebase.auth().createUserWithEmailAndPassword(email, password);
-//add name to user profile so we can say "hello x"
-        await user.updateProfile({ displayName: name })
+      id = add.id
 
+      await firestore.collection('groups')
+          .doc(id)
+          .collection('events')
+          .add({
+            location: 'Group Created',
+            summary: 'Group Created',
+            chosenDate: new Date().toString()
+          })
 
+      await firestore.collection('groups')
+          .doc(id)
+          .collection('users')
+          .add({ displayName, userID: uid, owner: true, admin: true, deleteID: id })
 
-
-
-export const newImage = (uri, mime = 'image/jpg') => {
-        //dispatch({  })
-        return dispatch => {
-        return new Promise((resolve, reject) => {
-        const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
-        const Blob = RNFetchBlob.polyfill.Blob
-        const fs = RNFetchBlob.fs
-        window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
-        window.Blob = Blob
-
-        let uploadBlob = null
-        const { currentUser } = firebase.auth();
-        const { uid } = currentUser;
-        const imageRef = firebase.storage().ref('/posts/${uid}')
-        fs.readFile(uploadUri, 'base64')
-        .then((data) => {
-        return Blob.build(data, { type: '${mime};BASE64' })
-        })
-        .then((blob) => {
-        uploadBlob = blob
-        return imageRef.put(blob, { contentType: mime })
-        })
-        .then(() => {
-        uploadBlob.close()
-        return imageRef.getDownloadURL()
-        })
-        .then((url) => {
-        currentUser.updateProfile({ photoURL : url })
-        console.log('done')
-        dispatch({
-        type: IMAGE_UPLOAD_WELCOME,
-        payload: url
-        })
-        })
-        .catch((error) => {
-        console.log(error)
-        console.log('thiscodesucks')
-        })
-        })
-        }
-        }
-
-
-
-
-        var { displayName, photoURL } =  firebase.auth().currentUser;
-        dispatch({
-        type: NAME_CHANGE_WELCOME,
-        payload: displayName
-        })
-        dispatch({
-        type: IMAGE_UPLOAD_WELCOME,
-        payload: photoURL
-        })
-
-
-
-        const response = await firebase.database().ref('/events/${this.state.data}')
-        .push({
-        location,
-        summary,
-        chosenDate: chosenDate.toString()
-        })
-        this.props.history.push('/getGroup/${this.state.data}')
-
-        group id//
-
+    
 */
