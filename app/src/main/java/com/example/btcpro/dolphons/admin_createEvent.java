c05d14277bf7945a53a0c082fd7c44fcab3b4cc4
nieroda <eathotlead@gmail.com>
2018-04-13 18:45:43 -0700

    new files

---
+++ app/src/main/java/com/example/btcpro/dolphons/admin_createEvent.java
@@ -0,0 +1,67 @@
+package com.example.btcpro.dolphons;
+
+import android.app.DatePickerDialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class admin_createEvent extends AppCompatActivity {
+
+    EditText edittext;
+    Calendar myCalendar = Calendar.getInstance();
+    String groupID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_create_event);
+
+        myCalendar = Calendar.getInstance();
+        groupID = getIntent().getExtras().getString("groupID");
+
+
+        edittext= (EditText) findViewById(R.id.Birthday);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        edittext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(admin_createEvent.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+
+    public void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+}
+
+// i have no clue if this works.
